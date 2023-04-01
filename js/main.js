3b8d4d34cf0ffe5274245dfeaea9705d39c27f11
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-04-01 00:30:41 -0400

    Add and conceal API key

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
-console.log('JS is working')
+const button = document.getElementById("123")
+button.addEventListener('click', event => {
+    const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Boston`;
+});
