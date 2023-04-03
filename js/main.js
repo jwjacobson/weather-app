71eabf1c8af34dea619f954601e0c612c6955509
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-04-02 21:41:52 -0400

    Add city name and local time to output

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ async function getapi(url) {
 
 function show(data) {
     let weather = 
-        `<ul>Current Weather:</ul>`;
+        `<ul class="mx-auto">Current Weather in ${data.location.name}<br> (as of ${data.current.last_updated} local time):</ul>`;
         weather += ` 
     <li class="list-group-item list-group-item-danger">Temp/Feels Like (C): ${data.current.temp_c}/${data.current.feelslike_c}</li>
     <li class="list-group-item list-group-item-success">Temp/Feels Like (F): ${data.current.temp_f}/${data.current.feelslike_f}</li>
@@ -24,10 +24,32 @@ form.addEventListener('submit', handleFormSubmit);
 
 async function handleFormSubmit(event){
     event.preventDefault();
-    let weatherCity = event.target.weatherCity.value;
+    var weatherCity = event.target.weatherCity.value;
     console.log(weatherCity);
     const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${weatherCity}`;
     getapi(url);
     event.target.weatherCity.value = '';
 }
-            
+   
+const select = document.getElementById('tempSelection');
+
+select.addEventListener('click', ({ target }) => { // handler fires on root container click
+  if (target.getAttribute('name') === 'option_gender') { // check if user clicks right element
+    alert('Filter by: ' + target.value);
+  }
+});
+
+
+// let radioC = document.getElementById('celsius');
+// form.addEventListener('option1', setCelsius);
+
+// let radioF = document.getElementById('fahrenheit');
+// form.addEventListener('option2', setFahrenheit);
+
+// async function setCelsius(event){
+//     var celsius = true;
+// }
+
+// async function setFahrenheit(event){
+//   var fahrenheit = true;
+// }
