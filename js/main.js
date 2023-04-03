29820e8c02e5aa532d2f603187b9c758f629911e
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2023-04-03 00:41:44 -0400

    Final touches

--- js/main.js
+++ js/main.js
@@ -1,22 +1,51 @@
+// radio button event listener adapted from https://stackoverflow.com/questions/49097300/add-event-listener-for-click-change-on-bootstrap-radio-buttons
+
+// temp is declared globally so the show() function below can use it
+var temp;
+
+// select the div containing the radio buttons
+const select = document.getElementById('tempSelection');
+
+// the value of the radio button selected is set as the value of temp, which determines which temperature format to show
+select.addEventListener('click', ({ target }) => { 
+  if (target.getAttribute('name') === 'tempFormat') { 
+    temp = target.getAttribute('value');
+  }
+});
+
 // getapi() and show() functions adapted from https://www.geeksforgeeks.org/how-to-use-the-javascript-fetch-api-to-get-data/#
- 
+
 async function getapi(url) {
     const response = await fetch(url);
     var data = await response.json();
-    console.log(data);
     show(data);
 }
 
 function show(data) {
+  // give the user a warning if they haven't selected fahrenheit or celsius
+    if (temp === undefined){
+      alert('Please select a temperature format.');
+    } else {
     let weather = 
-        `<ul class="mx-auto">Current Weather in ${data.location.name}<br> (as of ${data.current.last_updated} local time):</ul>`;
-        weather += ` 
-    <li class="list-group-item list-group-item-danger">Temp/Feels Like (C): ${data.current.temp_c}/${data.current.feelslike_c}</li>
-    <li class="list-group-item list-group-item-success">Temp/Feels Like (F): ${data.current.temp_f}/${data.current.feelslike_f}</li>
-    <li class="list-group-item list-group-item-warning">Conditions: ${data.current.condition.text}</li>
+        `<ul class="mx-auto"><h5 class="text text-info-emphasis">Current Weather in ${data.location.name}<br> (as of ${data.current.last_updated} local time)</h5></ul>`;
+    if (temp == "Celsius"){
+      weather += `
+      <li class="list-group-item list-group-item-danger text text-info-emphasis">Temperature: ${data.current.temp_c}</li>`;
+      weather +=
+      `<li class="list-group-item list-group-item-success text text-info-emphasis">Feels like: ${data.current.feelslike_c}</li>`;
+    } else {
+      weather += `
+    <li class="list-group-item list-group-item-danger text text-info-emphasis">Temperature: ${data.current.temp_f}</li>`;
+      weather += `
+    <li class="list-group-item list-group-item-success text text-info-emphasis">Feels like: ${data.current.feelslike_f}</li>`;
+    }
+      weather += `
+    <li class="list-group-item list-group-item-warning text text-info-emphasis">Conditions: ${data.current.condition.text}</li>
     `;
-    // Setting innerHTML as tab variable
+    // making border appear adapted from https://stackoverflow.com/questions/66680001/change-bootstrap-div-border-color-with-javascript
+    document.getElementById("cityWeather").setAttribute('style', 'border: 1px solid green');
     document.getElementById("cityWeather").innerHTML = weather;
+}
 }
 
 let form = document.getElementById('weatherForm');
@@ -30,26 +59,4 @@ async function handleFormSubmit(event){
     getapi(url);
     event.target.weatherCity.value = '';
 }
-   
-const select = document.getElementById('tempSelection');
-
-select.addEventListener('click', ({ target }) => { // handler fires on root container click
-  if (target.getAttribute('name') === 'option_gender') { // check if user clicks right element
-    alert('Filter by: ' + target.value);
-  }
-});
-
-
-// let radioC = document.getElementById('celsius');
-// form.addEventListener('option1', setCelsius);
-
-// let radioF = document.getElementById('fahrenheit');
-// form.addEventListener('option2', setFahrenheit);
-
-// async function setCelsius(event){
-//     var celsius = true;
-// }
-
-// async function setFahrenheit(event){
-//   var fahrenheit = true;
-// }
+ 
